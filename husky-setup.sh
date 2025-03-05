62c554ff94d431f49d2c6d1ec18cc64d689d72b1
Premo <ajaipremo@gmail.com>
2025-03-05 18:38:43 -0500

    feat(chore): added husky (uses docker)

--- husky-setup.sh
+++ husky-setup.sh
@@ -1,7 +1,11 @@
 # This tells your local github repo to run husky without the need to
 # install husky directly. I have created a docker image that will
-# run a containerized version of husky.
+# containerized version of husky.
 # TODO: RUN THIS SCRIPT IN EACH SERVICE. I HAVE PROVIDED THIS SCRIPT IN EACH
+
+# THESE COMMANDS DO NOT WORK IN WINDOWS POWERSHELL OR CMD...
+# THEY NEED TO BE RUN FROM GIT BASH TERMINAL IF ON WINDOWS.
+# IF ON LINUX OR MAC, THEY CAN BE RUN FROM TERMINAL.
 
 # To run this script, run this command on all OS'
 # (requires Git to be installed):
@@ -13,3 +17,6 @@ git config core.hooksPath .husky
 git config --get core.hooksPath
 
 # Output should be ".husky"
+
+# Additionally, the 'commit-msg' file in the .husky folder needs
+# to be executable in order to work.
